07aadb0e615ecd36461245341a8b62d0ddbff940
Eke Péter <peterekepeter@gmail.com>
2019-10-15 02:11:10 +0300

    Added tracking code.

---
+++ global.js
@@ -0,0 +1,11 @@
+(function (){
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    var a = 'UA-150069877-1';
+    gtag('config', a);
+    var scriptElement = document.createElement('script');
+    scriptElement.async = true;
+    scriptElement.src = 'https://www.googletagmanager.com/gtag/js?id='+a;
+    document.head.appendChild(scriptElement)
+})();
